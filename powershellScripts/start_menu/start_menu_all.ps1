1678bedec5bd764104c943c5f207efdb7271f0b0
Kaleb <113309697+kalebpc@users.noreply.github.com>
2024-12-10 21:22:54 -0500

    Update start_menu_all.ps1

--- powershellScripts/start_menu/start_menu_all.ps1
+++ powershellScripts/start_menu/start_menu_all.ps1
@@ -3,3 +3,5 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
 [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
 [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+# Open start menu, navigate to all apps list
