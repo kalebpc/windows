5dd9e04e16be0f6a70a42a1a16d4af49dbfc10aa
Kaleb <113309697+kalebpc@users.noreply.github.com>
2024-12-23 10:21:41 -0500

    Update InitializeXamlWindow.psm1 help comments

--- powershellScripts/forms/modules/InitializeXamlWindow/InitializeXamlWindow.psm1
+++ powershellScripts/forms/modules/InitializeXamlWindow/InitializeXamlWindow.psm1
@@ -1,10 +1,43 @@
+<#
+.SYNOPSIS
+
+Take xaml file and return [hashtable] containing [System.Windows.Window], [hashtable] objects.
+
+.PARAMETER Path
+
+Absolute or relative path to xaml file being used.
+
+.EXAMPLE
+
+Initialize-XamlWindow -Path "$Env:SystemDrive/Absolute/Path/to/XamlFileName.xaml"
+
+Name                           Value
+----                           -----
+window                         System.Windows.Window
+variables                      {xamlCheckbox5, xamlTextbox4, xamlTextbox6, xamlWindowControlMinimize...}
+
+.EXAMPLE
+
+$variable = Initialize-XamlWindow -Path "./Relative/Path/to/XamlFileName.xaml"
+
+$variable.variables
+
+Name                           Value
+----                           -----
+xamlCheckbox5                  System.Windows.Controls.CheckBox Content: IsChecked:False
+xamlTextbox4                   System.Windows.Controls.TextBox
+xamlTextbox6                   System.Windows.Controls.TextBox
+
+#>
+
 function Initialize-XamlWindow {
 [CmdletBinding()]
+[OutputType([psobject])]
 param (
+    [PARAMETER(Mandatory=$true)]
+    [ValidateScript({Test-Path -Path $_.trim()})]
     [string]$Path
     )
-
-if ( $(Try { Test-Path -Path $Path.trim() } Catch { $false }) ) {
 
     [string]$xamlContent = Get-Content $Path
     [string]$xamlContent = $xamlContent -replace 'mc:Ignorable="d"', '' -replace 'x:N', 'N' -replace 'x:Class=".*?"', ''
@@ -16,21 +49,10 @@ if ( $(Try { Test-Path -Path $Path.trim() } Catch { $false }) ) {
 
     $variables = @{}
 
-    ## Set env vars for named elements from xaml file
+    ## Set vars for named elements from xaml file
     $xmlNodes.SelectNodes("//*[@Name]") | ForEach-Object { Set-Variable -Name ($_.Name) -Value $window.FindName($_.Name) }
 
     Get-Variable xaml* | ForEach-Object { $variables[$_.Name] = $_.Value }
 
-    [hashtable]$Window = @{
-        window = [System.Windows.Window]$window
-        variables = [hashtable]$variables
-    }
-    return $Window
-}
-
-Else {
-    Write-Host "Could not locate xaml file at : $Path"
-    Return
-}
-
+    return @{window = [System.Windows.Window]$window; variables = [hashtable]$variables}
 }
