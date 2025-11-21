8acc0590e73e83c2fc402d223c7d60ea2b4c39d6
Existing Goat <113309697+kalebpc@users.noreply.github.com>
2025-11-21 11:43:49 -0500

    Update backuppc.bat for better backup handling
    Refactor backup script to improve structure and logging.

--- batchScripts/copyFilesFolders/backuppc.bat
+++ batchScripts/copyFilesFolders/backuppc.bat
@@ -1,139 +1,108 @@
 
-:: What this code does.
+@REM -- What this code does.
+@REM -- 1. Check and/or create Backup_PC folder in !USERPROFILE!\AppData\Local\Programs\ directory.
+@REM -- 2. Check and/or create logs directory in !USERPROFILE!\AppData\Local\Programs\Backup_PC directory.
+@REM -- 3. Check and/or create !BACKUPLISTFILE! in !USERPROFILE!\AppData\Local\Programs\Backup_PC directory.
+@REM -- 4. Check and/or create backup destination folder.
+@REM -- 5. Finds list of folders\files in !BACKUPLISTFILE! to copy.
+@REM -- 6. Copy directories and files to destination folder.
+@REM -- 7. Displays location of log file, exit message and exit code to terminal.
 
-:: 1. Create \logs folder in source directory and save log file of recent backups (once the file is created, if the backup name is the same logs append).
-
-:: 2. Look for backup.txt located in source directory. Create if not existing. Put instructions for listing folders\files in backup.txt
-
-:: 3. Finds listed folders\files in backup.txt and copies to backup directory.
-
-:: 4. Displays location of log file, exit message and exit code to terminal.
-
-:: ECHO /? for help
-
+@REM -- ECHO /? for help
 @ECHO OFF
-
-:: SETLOCAL /? for help
-
-SETLOCAL
-
-:: SET /? for help and DOS string extraction for ":~start,end"
-
-SET drive=%HOMEDRIVE:~0,1%
-
-SET source=%HOMEPATH%
-
-:: ********\/\/\/\/*********** ENTER BACKUP FOLDER NAME HERE (also changes name of log file)*******************
-SET backup=pcBackup
-
-
-:: \/\/\/\/\/\/ Setup\Verify log file location \/\/\/\/\/\/
-
-IF NOT EXIST %USERPROFILE%\logs\ (MKDIR %USERPROFILE%\logs)
-
-SET log=%USERPROFILE%\logs\%backup%.log
-
-:: CD /? for help
-
-CD %USERPROFILE%\logs
-
-ECHO ====== %DATE% %TIME% ====== >> %backup%.log
-
-:: /\/\/\/\/\/\ Setup\Verify log file location /\/\/\/\/\/\
-
-
-:: \/\/\/\/\/\/ Verify source and Setup/Verify destination \/\/\/\/\/\/
-
-:: IF /? for help
-
-SET /P destination=Enter backup drive ^( c ^| d ^| f ^| ... ^) : 
-
-IF /I %destination%==%drive% (ECHO Backup drive must not be same as source drive & COLOR 00 & GOTO end) ELSE (
-
-:: Checking for existing drive
-IF NOT EXIST %destination%:\ (ECHO The system cannot find the path specified. & COLOR 00 & GOTO end) ELSE (
-
-:: Checking for existing backup folder on drive. Create if not existing
-IF NOT EXIST %destination%:\%backup% (MKDIR %destination%:\%backup% )
-))
-
-SET destination=%destination%:\%backup%
-
-:: /\/\/\/\/\/\ Verify source and Setup/Verify destination /\/\/\/\/\/\
-
-:: \/\/\/\/\/\/ Setup/Verify backup.txt (List of folders\files to backup) \/\/\/\/\/\/
-
-CD %USERPROFILE%
-
-IF NOT EXIST %CD%\backup.txt (ECHO :: List folders\files from %USERPROFILE% you want backed up. > backup.txt & (
-
-ECHO :: **Example list**
-
-ECHO ::
-
-ECHO :: Documents\
-
-ECHO :: Downloads\
-
-ECHO :: Pictures\MyPictures\
-
-ECHO ::
-
-ECHO :: **LINES BEGINING WITH :: WILL NOT BE BACKED UP**
-
-ECHO :: **Start your list below**
-
-) >> backup.txt & START notepad %CD%\backup.txt & GOTO nopauseexit)
-
-:: /\/\/\/\/\/\ Setup/Verify backup.txt (List of folders\files to backup) /\/\/\/\/\/\
-
-:: FOR /? for help
-
-:: \/\/\/\/\/\/ Copy folders\files listed in backup.txt \/\/\/\/\/\/
-
-:: ROBOCOPY /? for help
-
-FOR /F "delims=" %%a IN ('FINDSTR /v :: %CD%\backup.txt') DO (ROBOCOPY "%source%\%%a " "%destination%\%%a " /MIR /LOG+:%log%)
-
-:: /\/\/\/\/\/\ Copy folders\files listed in backup.txt /\/\/\/\/\/\
-
-:: \/\/\/\/\/\/ Check for errors and print helpful info if needed \/\/\/\/\/\/
-
+@REM -- SETLOCAL /? for help
+SETLOCAL EnableDelayedExpansion
+SET INSTALLDIRNAME="Backup_PC"
+SET BACKUPDESTNAME="pcBackup"
+SET BACKUPLISTFILE="backup_list.txt"
+SET FILELOG="!BACKUPDESTNAME!.log"
+SET PATHINSTALLDIR="!USERPROFILE!\AppData\Local\Programs\!INSTALLDIRNAME:"=!"
+SET PATHLOGSDIR="!PATHINSTALLDIR:"=!\logs"
+SET PATHBACKUPLISTFILE="!PATHINSTALLDIR:"=!\!BACKUPLISTFILE:"=!"
+@REM -- Create !INSTALLDIRNAME! folder in !USERPROFILE!\AppData\Local\Programs\ directory.
+IF NOT EXIST !PATHINSTALLDIR! (
+    @REM -- User prompt
+    ECHO. & ECHO !PATHINSTALLDIR! does not exist. & ECHO.
+    SET /P AREYOUSURE="Do you want to create it now? (Y/[N])?"
+    IF /I "!AREYOUSURE!" NEQ "Y" GOTO END
+    MKDIR !PATHINSTALLDIR!
+    IF NOT EXIST !PATHINSTALLDIR! ( ECHO. & ECHO Could not create !PATHINSTALLDIR!. & GOTO END )
+)
+@REM -- Create logs directory in !USERPROFILE!\AppData\Local\Programs\Backup_PC directory.
+IF NOT EXIST !PATHLOGSDIR! (
+    @REM -- User prompt
+    ECHO. & ECHO !PATHLOGSDIR! does not exist. & ECHO.
+    SET /P AREYOUSURE="Do you want to create it now? (Y/[N])?"
+    IF /I "!AREYOUSURE!" NEQ "Y" GOTO END
+    MKDIR !PATHLOGSDIR!
+    IF NOT EXIST !PATHLOGSDIR! ( ECHO. & ECHO Could not create !PATHLOGSDIR!. & GOTO END )
+)
+@REM -- Create !BACKUPLISTFILE! in !USERPROFILE!\AppData\Local\Programs\Backup_PC directory.
+IF NOT EXIST !PATHBACKUPLISTFILE! (
+    @REM -- User prompt
+    ECHO. & ECHO !PATHBACKUPLISTFILE! does not exist. & ECHO.
+    SET /P AREYOUSURE="Do you want to create it now? (Y/[N])?"
+    IF /I "!AREYOUSURE!" NEQ "Y" GOTO END
+    GOTO MKBACKUPFILE
+)
+ECHO ====== %DATE% %TIME% ====== >> "!PATHLOGSDIR:"=!\!FILELOG:"=!"
+:SETDESTINATION
+SET /P USERDRIVEINPUT=Enter backup drive ^( c ^| d ^| f ^| ... ^) : 
+@REM -- IF /? for help
+IF /I !USERDRIVEINPUT!==!HOMEDRIVE::=! ( ECHO Backup drive must not be same as source drive. & GOTO SETDESTINATION )
+@REM -- Checking for drive existence
+IF NOT EXIST !USERDRIVEINPUT!:\ ( ECHO The system cannot find the path specified. & GOTO SETDESTINATION )
+SET DESTINATION=!USERDRIVEINPUT!:\!BACKUPDESTNAME:"=!
+@REM -- Checking for existing backup folder on backup drive. Create if not existing
+IF NOT EXIST !DESTINATION! ( MKDIR !DESTINATION! ) ELSE ( ECHO !DESTINATION! - Drive located. )
+@REM -- FOR /? for help
+@REM -- ROBOCOPY /? for help
+SET i=0
+FOR /f "delims=" %%A IN ('FINDSTR /V :: !PATHBACKUPLISTFILE!') DO (
+    SET TEMP=%%~dA
+    SET TEMP=!TEMP::=!
+    ROBOCOPY "%%A" "!DESTINATION!\!TEMP!%%~pnxA" /tee /e /z /xjd /LOG+:"!PATHLOGSDIR:"=!\%%~nA_!FILELOG:"=!"
+)
+@REM -- /e    : copies subdirs includes empty dirs.
+@REM -- /z    : Copies files in restartable mode.
+@REM -- /xx   : Excluding extra files doesn't delete files from the destination and suppress extra files on logs.
+@REM -- /LOG+ : Append to end of log file.
+ECHO.
 IF %errorlevel% GEQ 16 (ECHO Copy Failed. & GOTO end) ELSE (
-
 IF %errorlevel% GEQ 8 (ECHO Several files didn't copy. & GOTO end) ELSE (
-
 IF %errorlevel% EQU 7 (ECHO Files were copied, a file mismatch was present, and additional files were present. & GOTO end) ELSE (
-
 IF %errorlevel% EQU 6 (ECHO Additional files and mismatched files exist. No files were copied and no failures were encountered meaning that the files already exist in the destination directory. & GOTO end) ELSE (
-
 IF %errorlevel% EQU 5 (ECHO Some files were copied. Some files were mismatched. No failure was encountered. & GOTO end) ELSE (
-
 IF %errorlevel% EQU 3 (ECHO Some files were copied. Additional files were present. No failure was encountered. & GOTO end) ELSE (
-
 IF %errorlevel% EQU 2 (ECHO There are some additional files in the destination directory that aren't present in the source directory. No files were copied. & GOTO end) ELSE (
-
 IF %errorlevel% EQU 1 (ECHO All files were copied successfully. & GOTO end) ELSE (
-
 IF %errorlevel% EQU 0 (ECHO No files were copied. No failure was encountered. No files were mismatched. The files already exist in the destination directory; therefore, the copy operation was skipped.)
 ))))))))
-
-:: /\/\/\/\/\/\ Check for errors and print helpful info if needed /\/\/\/\/\/\
-
-:end
-
-ECHO Exit %ERRORLEVEL%
-
-:: ENDLOCAL /? for help
-
+GOTO END
+:MKBACKUPFILE
+MKDIR
+ECHO :: ** List fully qualified paths you want backed up. ** > !PATHBACKUPLISTFILE! & (
+    ECHO ::
+    ECHO :: ** LINES BEGINING WITH "::" WILL NOT BE BACKED UP **
+    ECHO ::
+    ECHO :: ** Start your list below **
+    ECHO !PATHINSTALLDIR:"=!
+    ECHO !USERPROFILE!\AppData
+    ECHO !USERPROFILE!\Desktop
+    ECHO !USERPROFILE!\Downloads
+    ECHO !USERPROFILE!\Documents
+    ECHO !USERPROFILE!\Music
+    ECHO !USERPROFILE!\Pictures
+    ECHO !USERPROFILE!\Videos 
+) >> !PATHBACKUPLISTFILE!
+IF EXIST !PATHBACKUPLISTFILE! ( START notepad !PATHBACKUPLISTFILE! & EXIT ) ELSE ( ECHO. & ECHO Could not create !PATHBACKUPLISTFILE!. )
+:END
+ECHO.
+ECHO Exit !ERRORLEVEL!
+ECHO List of backed up files/folders located at !PATHBACKUPLISTFILE!.
+@REM -- ENDLOCAL /? for help
 ENDLOCAL
-
-:: PAUSE /? for help
-
+@REM -- PAUSE /? for help
 PAUSE
-
-:nopauseexit
-
-:: EXIT /? for help
-
+@REM -- EXIT /? for help
 EXIT
