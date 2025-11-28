d1a08e4692ef5271c08e8e4138c90152e2ecee9f
Kalebpc <113309697+kalebpc@users.noreply.github.com>
2025-11-28 17:33:47 -0500

    Enhance backuppc.bat with user prompts and .env checks
    Updated the backuppc.bat script to include checks for .env file and user prompts for backup folder and list file names.

--- batchScripts/copyFilesFolders/backuppc.bat
+++ batchScripts/copyFilesFolders/backuppc.bat
@@ -14,55 +14,144 @@
 
 @REM -- ECHO /? for help
 @ECHO OFF
+ECHO Backuppc © 2025 https://github.com/kalebpc/windows
 @REM -- SETLOCAL /? for help
 SETLOCAL EnableDelayedExpansion
 SET THREADZ=4
-SET INSTALLDIRNAME="Backup_PC"
-SET BACKUPDESTNAME="pcBackup"
-SET BACKUPLISTFILE="backup_list.txt"
-@REM ** Added for mirror backup
-SET MIRBACKUPLISTFILE="mirror_backup_list.txt"
-SET FILELOG="!BACKUPDESTNAME!.log"
-SET PATHINSTALLDIR="!USERPROFILE!\AppData\Local\Programs\!INSTALLDIRNAME:"=!"
-SET PATHLOGSDIR="!PATHINSTALLDIR:"=!\logs"
-SET PATHBACKUPLISTFILE="!PATHINSTALLDIR:"=!\!BACKUPLISTFILE:"=!"
-@REM ** Added for mirror backup
-SET PATHMIRBACKUPLISTFILE="!PATHINSTALLDIR:"=!\!MIRBACKUPLISTFILE:"=!"
+SET "INSTALLDIRNAME=Backup_PC"
+SET "PATHINSTALLDIR=!USERPROFILE!\AppData\Local\Programs\!INSTALLDIRNAME!"
+SET "PATHENVFILE=!PATHINSTALLDIR!\.env"
+@REM -- Check for .env file. Set variables. Create .env file if it does not exist.
+IF NOT EXIST !PATHENVFILE! (
+    SET "BACKUPDESTNAME=pcBackup"
+    SET "BACKUPLISTFILE=backup_list.txt"
+    SET "MIRBACKUPLISTFILE=mirror_backup_list.txt"
+    SET "FILELOG=!BACKUPDESTNAME!.log"
+    SET "PATHLOGSDIR=!PATHINSTALLDIR!\logs"
+    SET "PATHBACKUPLISTFILE=!PATHINSTALLDIR!\!BACKUPLISTFILE!"
+    SET "PATHMIRBACKUPLISTFILE=!PATHINSTALLDIR!\!MIRBACKUPLISTFILE!"
+    ECHO ^|
+    CHOICE /C YN /T 15 /D N /M "Change backup folder name? Current name is '!BACKUPDESTNAME!'."
+    IF !ERRORLEVEL! EQU 1 (
+        :SETBACKUPDESTNAME
+        SET /p "BACKUPDESTNAME=Set Backup folder name : "
+        IF !ERRORLEVEL! EQU 1 ( GOTO SETBACKUPDESTNAME )
+    )
+    :CHANGEBACKUPLIST
+    ECHO ^|
+    CHOICE /C YN /T 15 /D N /M "Change backup list file? Current name is '!PATHBACKUPLISTFILE!'."
+    IF !ERRORLEVEL! EQU 1 (
+        @REM -- Open Savefiledialog to get name and path
+        @REM TODO ...
+        FOR /f "delims=" %%E IN (
+            'POWERSHELL "&{"^
+            "Add-Type -AssemblyName System.Windows.Forms;"^
+            "$saveFileDialog=New-Object System.Windows.Forms.SaveFileDialog;"^
+            "$saveFileDialog.Description='Choose Source Folder to Copy from.';"^
+            "$saveFileDialog.InitialDirectory=[Environment]::GetFolderPath([System.Environment+SpecialFolder]::LocalApplicationData);"^
+            "$saveFileDialog.Filter='Txt files (*.txt)|*.txt';"^
+            "$saveFileDialog.FilterIndex=2;"^
+            "$saveFileDialog.CheckFileExists=true;"^
+            "$saveFileDialog.CheckPathExists=true;"^
+            "$saveFileDialog.RestoreDirectory=true;"^
+            "$saveFileDialog.ShowDialog();"^
+            "ECHO $saveFileDialog.FileName"^
+            "}"'
+        ) DO SET "TEMPPATH=%%E"
+        IF /i "!TEMPPATH!" EQU "Cancel" ( 
+            CHOICE /C YN /T 15 /D N /M "Did you mean to Cancel?"
+            @REM -- May have to revisit this early exit in future
+            IF !ERRORLEVEL! EQU 1 ( EXIT ) ELSE GOTO CHANGEBACKUPLIST
+        )
+        SET "PATHBACKUPLISTFILE=!TEMPPATH!"
+        ECHO ^|
+        ECHO "Backup List File set : !PATHBACKUPLISTFILE!"
+        @REM -- Set PATHBACKUPLISTFILE from full path/filename
+        @REM TODO ...
+        @REM -- Set BACKUPLISTFILE from name
+        @REM TODO ...
+    )
+    :CHANGEMIRRORBACKUPLIST
+    ECHO ^|
+    CHOICE /C YN /T 15 /D N /M "Change MIRROR backup list file? Current name is '!PATHMIRBACKUPLISTFILE!'."
+    IF !ERRORLEVEL! EQU 1 (
+        @REM -- Open Savefiledialog to get name and path
+        @REM TODO ...
+        FOR /f "delims=" %%F IN (
+            'POWERSHELL "&{"^
+            "Add-Type -AssemblyName System.Windows.Forms;"^
+            "$saveFileDialog=New-Object System.Windows.Forms.SaveFileDialog;"^
+            "$saveFileDialog.Description='Choose Source Folder to Copy from.';"^
+            "$saveFileDialog.InitialDirectory=[Environment]::GetFolderPath([System.Environment+SpecialFolder]::LocalApplicationData);"^
+            "$saveFileDialog.Filter='Txt files (*.txt)|*.txt';"^
+            "$saveFileDialog.FilterIndex=2;"^
+            "$saveFileDialog.CheckFileExists=true;"^
+            "$saveFileDialog.CheckPathExists=true;"^
+            "$saveFileDialog.RestoreDirectory=true;"^
+            "$saveFileDialog.ShowDialog();"^
+            "ECHO $saveFileDialog.FileName"^
+            "}"'
+        ) DO SET "TEMPPATH=%%F"
+        IF /i "!TEMPPATH!" EQU "Cancel" ( 
+            CHOICE /C YN /T 15 /D N /M "Did you mean to Cancel?"
+            @REM -- May have to revisit this early exit in future
+            IF !ERRORLEVEL! EQU 1 ( EXIT ) ELSE GOTO CHANGEMIRRORBACKUPLIST
+        )
+        SET "PATHMIRBACKUPLISTFILE=!TEMPPATH!"
+        ECHO ^|
+        ECHO "Mirror Backup List File set : !PATHMIRBACKUPLISTFILE!"
+        @REM -- Set PATHMIRBACKUPLISTFILE from full path/filename
+        @REM TODO ...
+        @REM -- Set MIRBACKUPLISTFILE from name
+        @REM TODO ...
+    )
+    @REM -- Make .env file and save env vars to it.
+    @REM TODO ...
+) 
+@REM ELSE (
+@REM     @REM -- Retreive env vars
+@REM     @REM TODO ...
+@REM     @REM FOR /F "delims== tokens=1,* eol=#" %%I IN ("!PATHENVFILE!") DO SET "%%I=%%J" & ECHO "%%I=%%J"
+@REM )
 @REM -- Create !INSTALLDIRNAME! folder in !USERPROFILE!\AppData\Local\Programs\ directory.
 IF NOT EXIST !PATHINSTALLDIR! (
     @REM -- User prompt
-    ECHO. & ECHO !PATHINSTALLDIR! does not exist. & ECHO.
-    SET /P AREYOUSURE="Do you want to create it now? (Y/[N])?"
-    IF /I "!AREYOUSURE!" NEQ "Y" GOTO END
+    ECHO ^| & ECHO !PATHINSTALLDIR! does not exist. & ECHO ^|
+    CHOICE /C YNC /T 15 /D Y /M "Do you want to create it now?  ('Y'-Yes[Default] | 'N'-No | 'C'-Cancel)"
+    IF !ERRORLEVEL! NEQ 1 ( GOTO END )
+    SET ERRORLEVEL=0
     MKDIR !PATHINSTALLDIR!
-    IF NOT EXIST !PATHINSTALLDIR! ( ECHO. & ECHO Could not create !PATHINSTALLDIR!. & GOTO END )
+    IF NOT EXIST !PATHINSTALLDIR! ( ECHO ^| & ECHO Could not create !PATHINSTALLDIR!. & GOTO END )
 )
 @REM -- Create logs directory in !USERPROFILE!\AppData\Local\Programs\Backup_PC directory.
 IF NOT EXIST !PATHLOGSDIR! (
     @REM -- User prompt
-    ECHO. & ECHO !PATHLOGSDIR! does not exist. & ECHO.
-    SET /P AREYOUSURE="Do you want to create it now? (Y/[N])?"
-    IF /I "!AREYOUSURE!" NEQ "Y" GOTO END
+    ECHO ^| & ECHO !PATHLOGSDIR! does not exist. & ECHO ^|
+    CHOICE /C YNC /T 15 /D Y /M "Do you want to create it now?  ('Y'-Yes[Default] | 'N'-No | 'C'-Cancel)"
+    IF !ERRORLEVEL! NEQ 1 ( GOTO END )
+    SET ERRORLEVEL=0
     MKDIR !PATHLOGSDIR!
-    IF NOT EXIST !PATHLOGSDIR! ( ECHO. & ECHO Could not create !PATHLOGSDIR!. & GOTO END )
+    IF NOT EXIST !PATHLOGSDIR! ( ECHO ^| & ECHO Could not create !PATHLOGSDIR!. & GOTO END )
 )
 @REM -- Create !BACKUPLISTFILE! in !USERPROFILE!\AppData\Local\Programs\Backup_PC directory.
 IF NOT EXIST !PATHBACKUPLISTFILE! (
     @REM -- User prompt
-    ECHO. & ECHO !PATHBACKUPLISTFILE! does not exist. & ECHO.
-    SET /P AREYOUSURE="Do you want to create it now? (Y/[N])?"
-    IF /I "!AREYOUSURE!" NEQ "Y" GOTO END
+    ECHO ^| & ECHO !PATHBACKUPLISTFILE! does not exist. & ECHO ^|
+    CHOICE /C YNC /T 15 /D Y /M "Do you want to create it now?  ('Y'-Yes[Default] | 'N'-No | 'C'-Cancel)"
+    IF !ERRORLEVEL! NEQ 1 ( GOTO END )
+    SET ERRORLEVEL=0
     GOTO MKBACKUPFILE
 )
 @REM ** Added for mirror backup
 IF NOT EXIST !PATHMIRBACKUPLISTFILE! (
     @REM -- User prompt
-    ECHO. & ECHO !PATHMIRBACKUPLISTFILE! does not exist. & ECHO.
-    SET /P AREYOUSURE="Do you want to create it now? (Y/[N])?"
-    IF /I "!AREYOUSURE!" NEQ "Y" GOTO END
+    ECHO ^| & ECHO !PATHMIRBACKUPLISTFILE! does not exist. & ECHO ^|
+    CHOICE /C YNC /T 15 /D Y /M "Do you want to create it now?  ('Y'-Yes[Default] | 'N'-No | 'C'-Cancel)"
+    IF !ERRORLEVEL! NEQ 1 ( GOTO END )
+    SET ERRORLEVEL=0
     GOTO MKMIRBACKUPFILE
 )
-SET LASTBACKUP=!PATHLOGSDIR:"=!\LastBackup_!FILELOG:"=!
+SET "LASTBACKUP=!PATHLOGSDIR!\LastBackup_!FILELOG!"
 @REM -- Change the character encoding of the terminal shown
 @CHCP 65001 > !LASTBACKUP!
 FOR /f "tokens=*" %%A IN ('TIME /t') DO SET "BACKUPTIME=%%A"
@@ -72,29 +161,30 @@ FOR /f "tokens=*" %%A IN ('TIME /t') DO SET "BACKUPTIME=%%A"
     ECHO    Started  : !DATE! !BACKUPTIME!
 ) > !LASTBACKUP!
 :SETDESTINATION
-SET /P USERDRIVEINPUT=Enter backup drive ^( c ^| d ^| f ^| ... ^) : 
+ECHO ^|
+SET /P "USERDRIVEINPUT=Enter backup drive ( c | d | f | ... ) : "
 @REM -- IF /? for help
 IF /I !USERDRIVEINPUT!==!HOMEDRIVE::=! ( ECHO Backup drive must not be same as source drive. & GOTO SETDESTINATION )
 @REM -- Checking for drive existence
 IF NOT EXIST !USERDRIVEINPUT!:\ ( ECHO The system cannot find the path specified. & GOTO SETDESTINATION )
-SET DESTINATION=!USERDRIVEINPUT!:\!BACKUPDESTNAME:"=!
+SET "DESTINATION=!USERDRIVEINPUT!:\!BACKUPDESTNAME!"
 @REM -- Checking for existing backup folder on backup drive. Create if not existing
 IF NOT EXIST !DESTINATION! ( MKDIR !DESTINATION! ) ELSE ( ECHO !DESTINATION!    Drive located. )
 @REM -- FOR /? for help
 @REM -- ROBOCOPY /? for help
 FOR /f "delims=" %%B IN ('FINDSTR /V :: !PATHBACKUPLISTFILE!') DO (
-    SET TEMP=%%~dB
-    SET TEMP=!TEMP::=!
-    ROBOCOPY "%%B" "!DESTINATION!\!TEMP!%%~pnxB" /tee /e /mt:!THREADZ! /z /xjd /unicode /unilog+:"!PATHLOGSDIR:"=!\%%~nB_!FILELOG:"=!"
+    SET "TEMP=%%~dB"
+    SET "TEMP=!TEMP::=!"
+    ROBOCOPY "%%B" "!DESTINATION!\!TEMP!%%~pnxB" /tee /e /mt:!THREADZ! /z /xjd /unicode /unilog+:"!PATHLOGSDIR!\%%~nB_!FILELOG!"
 )
 @REM -- /e    : copies subdirs includes empty dirs.
 @REM -- /z    : Copies files in restartable mode.
 @REM -- /unilog+ : Append to end of unicode log file.
 @REM ** Added for mirror backup
 FOR /f "delims=" %%C IN ('FINDSTR /V :: !PATHMIRBACKUPLISTFILE!') DO (
-    SET TEMP=%%~dC
-    SET TEMP=!TEMP::=!
-    ROBOCOPY "%%C" "!DESTINATION!\!TEMP!%%~pnxC" /tee /mt:!THREADZ! /z /mir /xjd /unicode /unilog+:"!PATHLOGSDIR:"=!\%%~nC_!FILELOG:"=!"
+    SET "TEMP=%%~dC"
+    SET "TEMP=!TEMP::=!"
+    ROBOCOPY "%%C" "!DESTINATION!\!TEMP!%%~pnxC" /tee /mt:!THREADZ! /z /mir /xjd /unicode /unilog+:"!PATHLOGSDIR!\%%~nC_!FILELOG!"
 )
 @REM -- /mir  : Mirror destination to source. This WILL delete extra files in destination.
 FOR /f "tokens=*" %%D IN ('TIME /t') DO SET "BACKUPTIME=%%D"
@@ -102,36 +192,35 @@ FOR /f "tokens=*" %%D IN ('TIME /t') DO SET "BACKUPTIME=%%D"
     ECHO.
     ECHO -------------------------------------------------------------------------------
 ) >> !LASTBACKUP!
-ECHO.
-IF !ERRORLEVEL! GEQ 16 (ECHO "Copy Failed." & GOTO end) ELSE (
-IF !ERRORLEVEL! GEQ 8 (ECHO Several files didn't copy. & GOTO end) ELSE (
-IF !ERRORLEVEL! EQU 7 (ECHO Files were copied, a file mismatch was present, and additional files were present. & GOTO end) ELSE (
-IF !ERRORLEVEL! EQU 6 (ECHO Additional files and mismatched files exist. No files were copied and no failures were encountered meaning that the files already exist in the destination directory. & GOTO end) ELSE (
-IF !ERRORLEVEL! EQU 5 (ECHO Some files were copied. Some files were mismatched. No failure was encountered. & GOTO end) ELSE (
-IF !ERRORLEVEL! EQU 3 (ECHO Some files were copied. Additional files were present. No failure was encountered. & GOTO end) ELSE (
-IF !ERRORLEVEL! EQU 2 (ECHO There are some additional files in the destination directory that aren't present in the source directory. No files were copied. & GOTO end) ELSE (
-IF !ERRORLEVEL! EQU 1 (ECHO All files were copied successfully. & GOTO end) ELSE (
-IF !ERRORLEVEL! EQU 0 (ECHO No files were copied. No failure was encountered. No files were mismatched. The files already exist in the destination directory; therefore, the copy operation was skipped.)
-))))))))
+ECHO ^|
+IF !ERRORLEVEL! GEQ 16 ( ECHO "Copy Failed." & GOTO END )
+IF !ERRORLEVEL! GEQ 8 ( ECHO Several files didn't copy. & GOTO END )
+IF !ERRORLEVEL! EQU 7 ( ECHO Files were copied, a file mismatch was present, and additional files were present. & GOTO END )
+IF !ERRORLEVEL! EQU 6 ( ECHO Additional files and mismatched files exist. No files were copied and no failures were encountered meaning that the files already exist in the destination directory. & GOTO END )
+IF !ERRORLEVEL! EQU 5 ( ECHO Some files were copied. Some files were mismatched. No failure was encountered. & GOTO END )
+IF !ERRORLEVEL! EQU 3 ( ECHO Some files were copied. Additional files were present. No failure was encountered. & GOTO END )
+IF !ERRORLEVEL! EQU 2 ( ECHO There are some additional files in the destination directory that aren't present in the source directory. No files were copied. & GOTO END )
+IF !ERRORLEVEL! EQU 1 ( ECHO All files were copied successfully. & GOTO END )
+IF !ERRORLEVEL! EQU 0 ( ECHO "No files were copied. No failure was encountered. No files were mismatched. The files already exist in the destination directory; therefore, the copy operation was skipped." )
 GOTO END
 :MKBACKUPFILE
+@REM ************ Add choice for putting user chosen folders in file instead of hard coded ones ************
+@REM TODO ... 
 (   ECHO :: ** List fully qualified paths you want backed up. **
     ECHO ::
     ECHO :: ** LINES BEGINING WITH ":: " WILL NOT BE BACKED UP **
     ECHO ::
     ECHO :: ** Start your list below **
     ECHO !PATHINSTALLDIR:"=!
-    ECHO !USERPROFILE!\AppData
-    ECHO !USERPROFILE!\Desktop
-    ECHO !USERPROFILE!\Downloads
-    ECHO !USERPROFILE!\Documents
     ECHO !USERPROFILE!\Music
     ECHO !USERPROFILE!\Pictures
     ECHO !USERPROFILE!\Videos 
 ) >> !PATHBACKUPLISTFILE!
-IF EXIST !PATHBACKUPLISTFILE! ( START notepad !PATHBACKUPLISTFILE! ) ELSE ( ECHO. & ECHO Could not create !PATHBACKUPLISTFILE!. )
+IF EXIST !PATHBACKUPLISTFILE! ( START notepad !PATHBACKUPLISTFILE! ) ELSE ( ECHO ^| & ECHO Could not create !PATHBACKUPLISTFILE!. )
 @REM ** Added for mirror backup
 :MKMIRBACKUPFILE
+@REM ************ Add choice for putting user chosen folders in file instead of hard coded ones ************
+@REM TODO ... 
 IF NOT EXIST !PATHMIRBACKUPLISTFILE! (
     (   ECHO :: ** List fully qualified paths you want backed up. **
         ECHO ::
@@ -147,10 +236,10 @@ IF NOT EXIST !PATHMIRBACKUPLISTFILE! (
         ECHO !USERPROFILE!\Documents
         ECHO !PATHINSTALLDIR:"=!
     ) >> !PATHMIRBACKUPLISTFILE!
-    IF EXIST !PATHMIRBACKUPLISTFILE! ( START notepad !PATHMIRBACKUPLISTFILE! & EXIT ) ELSE ( ECHO. & ECHO Could not create !PATHMIRBACKUPLISTFILE!. )
+    IF EXIST !PATHMIRBACKUPLISTFILE! ( START notepad !PATHMIRBACKUPLISTFILE! & EXIT ) ELSE ( ECHO ^| & ECHO Could not create !PATHMIRBACKUPLISTFILE!. )
 )
 :END
-ECHO.
+ECHO ^|
 ECHO Exit !ERRORLEVEL!
 ECHO List of backed up files/folders located at !PATHBACKUPLISTFILE!.
 ECHO List of Mirrored files/folders located at  !PATHMIRBACKUPLISTFILE!.
